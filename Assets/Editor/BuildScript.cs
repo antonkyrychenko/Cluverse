d8625108918a8a680b9450466765aa70c30c7f63
anton.kyrychenko <anton.kyrychenko11@gmail.com>
2026-02-09 10:57:47 +0200

    Change yaml

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -1,41 +1,15 @@
-using System.Linq;
+// Assets/Editor/BuildScript.cs
 using UnityEditor;
-using UnityEditor.Build.Reporting;
 
-public static class BuildScript
+public class BuildScript
 {
-    public static void BuildIos()
-    {
-        // Force iOS target (important for CI)
-        EditorUserBuildSettings.SwitchActiveBuildTarget(
-            BuildTargetGroup.iOS,
-            BuildTarget.iOS
-        );
-
-        string path = "ios";
-
-        string[] scenes = EditorBuildSettings.scenes
-            .Where(s => s.enabled)
-            .Select(s => s.path)
-            .ToArray();
-
-        BuildPlayerOptions options = new BuildPlayerOptions
-        {
-            scenes = scenes,
-            locationPathName = path,
-            target = BuildTarget.iOS,
-            options = BuildOptions.None
-        };
-
-        BuildReport report = BuildPipeline.BuildPlayer(options);
-
-        if (report.summary.result != BuildResult.Succeeded)
-        {
-            throw new System.Exception(
-                $"iOS build failed: {report.summary.result}"
-            );
-        }
-
-        UnityEngine.Debug.Log("✅ iOS Xcode project generated at /ios");
-    }
+  public static void BuildiOS()
+  {
+    BuildPipeline.BuildPlayer(
+        EditorBuildSettings.scenes,
+        "ios",
+        BuildTarget.iOS,
+        BuildOptions.None
+    );
+  }
 }
