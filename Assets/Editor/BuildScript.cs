e376b5e66029b73ef74969be7f15e00be69785fa
anton.kyrychenko <anton.kyrychenko11@gmail.com>
2026-02-09 14:51:28 +0200

    Add some logs

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -1,15 +1,29 @@
 // Assets/Editor/BuildScript.cs
+using System.IO;
 using UnityEditor;
 
 public class BuildScript
 {
   public static void BuildiOS()
   {
+    string buildPath = Path.Combine(System.Environment.CurrentDirectory, "ios");
+    if (!Directory.Exists(buildPath))
+    {
+      UnityEngine.Debug.Log("Build directory doesn't exist. Creating new one");
+      Directory.CreateDirectory(buildPath);
+    }
+
     BuildPipeline.BuildPlayer(
         EditorBuildSettings.scenes,
-        "ios",
+        buildPath,
         BuildTarget.iOS,
         BuildOptions.None
     );
+
+    UnityEngine.Debug.Log("Xcode project generated at: " + buildPath);
+    foreach (var file in Directory.GetFiles(buildPath, "*", SearchOption.AllDirectories))
+    {
+      UnityEngine.Debug.Log(file);
+    }
   }
 }
